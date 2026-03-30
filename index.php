00ea39fe428a5047c03e79bf06c832d3033d0662
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-30 12:00:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_set_cookie_params(0, '/', 'house-778.theorangecow.org');
 session_start();
 define('BASE_PATH', __DIR__ . '/../');
