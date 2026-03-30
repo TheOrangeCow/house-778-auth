0f7789e6d933df79b9c8eba5b6f19313c9a56c60
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-30 12:05:15 +0100

    update

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@ if (file_exists(__DIR__ . '/.env')) {
 $host = "127.0.0.1:3306";
 $user = getenv('db_user');
 $pass = getenv('db_pass');
-$db = "user";
+$db = "users";
 
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
