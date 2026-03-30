96f215d6f8aa583164b9a119d500253d6bad67fa
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-30 12:14:29 +0100

    update

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+$cookieParams = session_get_cookie_params();
+session_set_cookie_params([
+    'lifetime' => $cookieParams["lifetime"],
+    'path' => $cookieParams["path"],
+    'domain' => '.theorangecow.org',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
 session_start();
 
 
